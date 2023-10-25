d2ed4a150bc215128794105732fbae52d8c15af9
Merten Fermont <merten.fermont@gmail.com>
2023-10-25 20:05:38 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+base_products = ['Mosselen', 'Balletjes', 'Scoutsbootje', 'Goulash', 'Frietjes']
+
+api_url = "https://api.orderbilly.com/devices/orders?status_codes=1,2,3"
+auth_key = 'ab98c878c25ef88ccf323dc16e9aabc344aaaba3c994b586612bd9fd2fa40f03'
