f280abbfd66ad164fea2d3117ec64032d3a413d9
Merten Fermont <merten.fermont@gmail.com>
2023-11-04 12:48:32 +0100

    initial commit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-base_products = ['Mosselen', 'Balletjes', 'Scoutsbootje', 'Goulash', 'Frietjes']
+base_products = ['Mosselen', 'Balletjes', 'Scoutsbootje', 'Goulash']
 
 api_url = "https://api.orderbilly.com/devices/orders?status_codes=1,2,3"
 auth_key = 'ab98c878c25ef88ccf323dc16e9aabc344aaaba3c994b586612bd9fd2fa40f03'
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import time
+#!/usr/bin/python
 
+import time
+import threading
+from datetime import datetime
+
+from kivy.graphics import Color, Rectangle
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty, StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.progressbar import ProgressBar
 
@@ -7,42 +14,70 @@ import config
 import billy
 
 import kivy
-kivy.require('2.2.1')
+kivy.require('2.1.0')
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.actionbar import ActionBar
-from kivy.clock import Clock
+from kivy.clock import Clock, mainthread
+
+from kivy.core.window import Window
+
+SCALE = 0.5
+Window.size = (1024*SCALE, 1280*SCALE)
+
+
+class ProductLabel(Label):
+    pass
+
+
+class AmountLabel(Label):
+    pass
 
 
 class OrderDashboard(GridLayout):
-    font_size = 60
+    font_size = 110*SCALE
+    amount_width = 290*SCALE
+    padding_sides = int(40 * SCALE)
+    totals_height = 400*SCALE
     cols = 2
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.thread = None
 
         self.labels_amount = {}
         for product in config.base_products:
-            self.add_widget(Label(text=product, font_size=self.font_size))
-            amount = Label(text='0', font_size=self.font_size)
+            label = ProductLabel(
+                text=product,
+                font_size=self.font_size,
+                padding=(self.padding_sides, 0, 0, 0)
+            )
+            #label.bind(size=label.setter('text_size'))
+            self.add_widget(label)
+
+            amount = AmountLabel(text='0', font_size=self.font_size, size_hint_x=None, width=self.amount_width, padding=(0, 0, self.padding_sides, 0))
             self.labels_amount[product] = amount
             self.add_widget(amount)
 
-        self.add_widget(Label(text='Totaal', font_size=self.font_size+10))
-        total = Label(text='0', font_size=self.font_size+10)
+        self.add_widget(ProductLabel(text='Totaal', font_size=self.font_size, padding=(self.padding_sides, 0, 0, 0), size_hint_y=None, height=self.totals_height))
+        total = AmountLabel(text='0', font_size=self.font_size, size_hint_x=None, width=self.amount_width, padding=(0, 0, self.padding_sides, 0), size_hint_y=None, height=self.totals_height)
         self.labels_amount['Totaal'] = total
         self.add_widget(total)
 
-        self.update_amounts()  # initialise values
+        self.start_update_thread()  # initialise values
 
-    def update(self, dt):
-        print(f'Update! {dt}')
-        self.update_amounts()
+    def start_update_thread(self, dt=None):
+        threading.Thread(target=self.update_thread).start()
 
-    def update_amounts(self):
+    def update_thread(self):
         ordered_products = billy.get_product_count()
-        time.sleep(1)
+        self.update_amounts(ordered_products)
+
+    @mainthread
+    def update_amounts(self, ordered_products):
+
+        print(ordered_products)
 
         for product, amount in ordered_products.items():
             self.labels_amount[product].text = str(amount)
@@ -50,42 +85,55 @@ class OrderDashboard(GridLayout):
 
 
 class StatusBar(BoxLayout):
-    def __int__(self):
-        super().__init__()
-        self.orientation = 'horizonal'
-
+    clock = StringProperty('xx:xx:xx')
+    
+    def set_time(self, dt=None):
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        self.clock = current_time
 
 class RefreshBar(ProgressBar):
-
-    def __int__(self):
-        self.max = 10
-        super().__init__()
-        
     def update(self, dt):
         self.value += dt
         print(f'{self.value} {self.max}')
 
+
+
 class DashboardApp(App):
+    thread = None
+    
     def build(self):
         layout = BoxLayout(orientation='vertical')
-        
+
         status = StatusBar()
         layout.add_widget(status)
-        
-        dashboard = OrderDashboard()
-        layout.add_widget(dashboard)
-        Clock.schedule_interval(dashboard.update, 5)
-        
-        self.progress = ProgressBar(max=10)
+        Clock.schedule_interval(status.set_time, 0.1)
+
+        self.dashboard = OrderDashboard()
+        layout.add_widget(self.dashboard)
+        Clock.schedule_interval(self.start_update_thread, 7)
+
+        self.progress = ProgressBar(max=10, size_hint_y=None, height=50*SCALE)
         layout.add_widget(self.progress)
-        Clock.schedule_interval(self.update_progress, 0.1)
-                
+        Clock.schedule_interval(self.update_progress, 1/60)
+
         return layout
-    
-    
+
+
     def update_progress(self, dt):
         self.progress.value += dt
 
+
+    def start_update_thread(self, dt=None):
+        if self.thread and self.thread.is_alive():
+            print('Previous thread is still running!')
+            return
+        self.thread = threading.Thread(target=self.update)
+        self.thread.start()
+
+    def update(self):
+        self.dashboard.update_thread()
+        self.progress.value = 0
 
 if __name__ == '__main__':
     DashboardApp().run()
